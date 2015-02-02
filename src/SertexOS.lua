efb4d9fca057a18f0d867a83cf89bb73ce3f17d5
Ale2610 <Ale2610>
2015-02-02 21:07:23 +0100

    test

--- src/SertexOS.lua
+++ src/SertexOS.lua
@@ -1,3 +1,4 @@
+sleep(0.1)
 local args = {...}
 
 local dbUsersDir = "/.SertexOS/databaseUsers/"
@@ -123,12 +124,12 @@ function login()
 		write( "   Password > " )
 		p = read( "*" )
 		encryptedPassword = sha256.sha256(p)
-		if not fs.exists(dbUsersDir.." "..u) then
+		if not fs.exists(dbUsersDir..u) then
 			print("Username not registered!")
 			sleep(2)
 			login()
 		end
-		f = fs.open( dbUsersDir.." "..u, "r" )
+		f = fs.open( dbUsersDir..u, "r" )
 		p2 = f.readLine()
 		f.close()
 		if encryptedPassword == p2 then
@@ -144,115 +145,117 @@ function login()
 	
 end
 
+login()
+
 -- desktop
 
 function desktop()
-	local termW, termH = term.getSize()
+	while true do
+		local termW, termH = term.getSize()
 
-	local sidebar = {
-		{"Shutdown", function()
-			local function printMsg(color)
-				term.setBackgroundColor(color)
-				term.setTextColor(colors.white)
+		local sidebar = {
+			{"Shutdown", function()
+				local function printMsg(color)
+					term.setBackgroundColor(color)
+					term.setTextColor(colors.white)
+					term.clear()
+					term.setCursorPos(1, 1)
+					sertextext.centerDisplay("Shutting Down...")
+					sleep(0.1)
+				end
+				printMsg(colors.white)
+				printMsg(colors.lightGray)
+				printMsg(colors.gray)
+				printMsg(colors.black)
+				sleep(0.6)
+				os.shutdown()
+			end},
+			{"Reboot", function()
+				local function printMsg(color)
+					term.setBackgroundColor(color)
+					term.setTextColor(colors.white)
+					term.clear()
+					term.setCursorPos(1, 1)
+					sertextext.centerDisplay("Rebooting...")
+					sleep(0.1)
+				end
+				printMsg(colors.white)
+				printMsg(colors.lightGray)
+				printMsg(colors.gray)
+				printMsg(colors.black)
+				sleep(0.6)
+				os.reboot()
+			end},
+			{"Logout", function()
+				local function printMsg(color)
+					term.setBackgroundColor(color)
+					term.setTextColor(colors.white)
+					term.clear()
+					term.setCursorPos(1, 1)
+					sertextext.centerDisplay("Logging Out...")
+					sleep(0.1)
+				end
+				printMsg(colors.white)
+				printMsg(colors.lightGray)
+				printMsg(colors.gray)
+				printMsg(colors.black)
+				sleep(0.6)
+				login()
+			end},
+			{"Shell", function()
+				term.setBackgroundColor(colors.black)
 				term.clear()
-				term.setCursorPos(1, 1)
-				sertextext.centerDisplay("Shutting Down...")
+				term.setCursorPos(1,1)
+				shell.run("/rom/programs/shell")
 				sleep(0.1)
+			end},
+		}
+	
+		local sidebarVisible = false
+		local sidebarWidth = 0
+		for i, v in ipairs(sidebar) do
+			if v[1]:len() > sidebarWidth then
+				sidebarWidth = v[1]:len()
 			end
-			printMsg(colors.white)
-			printMsg(colors.lightGray)
-			printMsg(colors.gray)
-			printMsg(colors.black)
-			sleep(0.6)
-			os.shutdown()
-		end},
-		{"Reboot", function()
-			local function printMsg(color)
-				term.setBackgroundColor(color)
-				term.setTextColor(colors.white)
-				term.clear()
-				term.setCursorPos(1, 1)
-				sertextext.centerDisplay("Rebooting...")
-				sleep(0.1)
-			end
-			printMsg(colors.white)
-			printMsg(colors.lightGray)
-			printMsg(colors.gray)
-			printMsg(colors.black)
-			sleep(0.6)
-			os.reboot()
-		end},
-		{"Logout", function()
-			local function printMsg(color)
-				term.setBackgroundColor(color)
-				term.setTextColor(colors.white)
-				term.clear()
-				term.setCursorPos(1, 1)
-				sertextext.centerDisplay("Logging Out...")
-				sleep(0.1)
-			end
-			printMsg(colors.white)
-			printMsg(colors.lightGray)
-			printMsg(colors.gray)
-			printMsg(colors.black)
-			sleep(0.6)
-			login()
-		end},
-		{"Shell", function()
-			term.setBackgroundColor(colors.black)
+		end
+
+		local function redraw()
+			term.setBackgroundColor(colors.white)
 			term.clear()
 			term.setCursorPos(1,1)
-			shell.run("/rom/programs/shell")
-			sleep(0.1)
-		end},
-	}
-	
-	local sidebarVisible = false
-	local sidebarWidth = 0
-	for i, v in ipairs(sidebar) do
-		if v[1]:len() > sidebarWidth then
-			sidebarWidth = v[1]:len()
-		end
-	end
-
-	local function redraw()
-		term.setBackgroundColor(colors.white)
-		term.clear()
-		term.setCursorPos(1,1)
-		term.setTextColor(colors.red)
-		graphics.line(termW, 1, termW, termH, colors.red)
-		term.setCursorPos(termW, math.ceil(termH / 2))
-		term.setTextColor(colors.white)
-		if sidebarVisible then
-			graphics.box(termW - sidebarWidth - 5, 1, termW - sidebarWidth - 3, termH, colors.orange)
-			graphics.box(termW - sidebarWidth - 2, 1, termW - 1, termH, colors.red)
-			write(">")
-			for i, v in ipairs(sidebar) do
-				term.setCursorPos(termW - sidebarWidth, i + 1)
-				write(v[1])
+			term.setTextColor(colors.red)
+			graphics.line(termW, 1, termW, termH, colors.red)
+			term.setCursorPos(termW, math.ceil(termH / 2))
+			term.setTextColor(colors.white)
+			if sidebarVisible then
+				graphics.box(termW - sidebarWidth - 5, 1, termW - sidebarWidth - 3, termH, colors.orange)
+				graphics.box(termW - sidebarWidth - 2, 1, termW - 1, termH, colors.red)
+				write(">")
+				for i, v in ipairs(sidebar) do
+					term.setCursorPos(termW - sidebarWidth, i + 1)
+					write(v[1])
+				end
+			else
+				write("<")
 			end
-		else
-			write("<")
 		end
-	end
-	while true do
-		redraw()
-		local ev = {os.pullEventRaw()}
-		if ev[1] == "terminate" then
-			return
-		elseif ev[1] == "mouse_click" then
-			if ev[3] == termW then
-				sidebarVisible = not sidebarVisible
-			elseif ev[3] >= termW - sidebarWidth
-			and    ev[3] <= termW - 1
-			and    ev[4] >= 2
-			and    sidebarVisible then
-				if sidebar[ev[4] - 1] then
-					sidebar[ev[4] - 1][2]()
+		while true do
+			redraw()
+			local ev = {os.pullEventRaw()}
+			if ev[1] == "terminate" then
+				return
+			elseif ev[1] == "mouse_click" then
+				if ev[3] == termW then
+					sidebarVisible = not sidebarVisible
+				elseif ev[3] >= termW - sidebarWidth
+				and    ev[3] <= termW - 1
+				and    ev[4] >= 2
+				and    sidebarVisible then
+					if sidebar[ev[4] - 1] then
+						sidebar[ev[4] - 1][2]()
+					end
 				end
 			end
 		end
 	end
 end
-
-login()
