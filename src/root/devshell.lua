ff13b96688cb21a5efcc659b175db2dd960ad461
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-04-23 13:10:19 +0100

    Create devshell.lua

---
+++ src/root/devshell.lua
@@ -0,0 +1,32 @@
+print( "SertexOS Developer Shell" )
+print( "You opened this window by typing \"devshell\" in the main" )
+print( "Operating System. If you don't know what this is, please" )
+print( "type \"exit\". Type \"help\" for more commands." )
+while true do
+  term.setTextColor( colors.blue )
+  write( "SertexOS@Root$ " )
+  input = read()
+  if input == "motd" then
+    print( "SertexOS Developer Shell" )
+    print( "You opened this window by typing \"devshell\" in the main" )
+    print( "Operating System. If you don't know what this is, please" )
+    print( "type \"exit\". Type \"help\" for more commands." )
+  elseif input == "exit" then
+    return true
+  elseif input == "help" then
+    print([[
+       _____           _             ____   _____ 
+       / ____|         | |           / __ \ / ____|
+       | (___   ___ _ __| |_ _____  _| |  | | (___  
+        \___ \ / _ \ '__| __/ _ \ \/ / |  | |\___ \ 
+       ____) |  __/ |  | ||  __/>  <| |__| |____) |
+      |_____/ \___|_|   \__\___/_/\_\\____/|_____/ 
+                                              
+      You are in Dev Shell
+      
+      Please use the exit command to return to the operating system.
+      
+      If you are reading this then you are NOT a developer.
+    ]])
+  
+end
