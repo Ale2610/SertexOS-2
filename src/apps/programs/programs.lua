becd1e06490813a45383868ea731659d4ca460bd
Ale2610 <ale.proto00@gmail.com>
2015-06-25 20:48:25 +0200

    Update programs.lua

--- src/apps/programs/programs.lua
+++ src/apps/programs/programs.lua
@@ -16,6 +16,26 @@ local function clear()
 	term.setTextColor(colors.red)
 end
 
+local function header()
+	local w,h = term.getSize()
+	graphics.line(1,1,w,1, colors.red)
+	term.setTextColor(colors.white)
+end
+
+local function header_exit(char)
+	if not char then
+		char = "X"
+	emd
+	header()
+	local w, h = term.getSize()
+	term.setCursorPos(w,1)
+	term.setBackgroundColor(colors.red)
+	write(char)
+	term.setBackgroundColor(colors.white)
+	term.setTextColor(colors.red)
+	term.setCursorPos(1,3)
+end
+
 local function draw()
 	clear()
 	local w,h = term.getSize()
@@ -35,6 +55,52 @@ local function draw()
 	term.setTextColor(colors.red)
 end
 
+local function add()
+	term.setBackgroundColor(colors.white)
+	term.clear()
+	header_exit("<")
+	write("File Directory: \")
+	local w,h = term.getSize()
+	local e =  { os.pullEvent() }
+	if e[1] == "mouse_click" or e[1] == "monitor_touch" then
+		local x = e[3]
+		local y = e[4]
+		if y == 1 and x == w then
+			return
+		end
+	elseif e[1] == "key" then
+		local dir = read()
+		write("Link Name: ")
+		local link = read()
+		local f = fs.open("/.SertexOS/programs/"..link, "w")
+		f.write("dofile(\"link\")")
+		f.close()
+		print("Done!")
+		print("Press Any Key")
+		os.pullEvent("key")
+		return
+	end
+end
+
+local function remove()
+	term.setBackgroundColor(colors.white)
+	term.clear()
+	header_exit("<")
+	local w,h = term.getSize()
+	local pList = fs.list("/.SertexOS/programs")
+	for i = 1, #pList do
+		print(pList[i])
+	end
+	local e =  { os.pullEvent() }
+	if e[1] == "mouse_click" or e[1] == "monitor_touch" then
+		local x = e[3]
+		local y = e[4]
+		if y == 1 and x == w then
+			return
+		end
+	end
+end
+
 draw()
 
 while true do
@@ -50,9 +116,9 @@ while true do
 			if x == w then
 				return
 			elseif x == 1 then
-				
+				add()
 			elseif x == 2 then
-				
+				remove()
 			end
 		else
 			shell.run("fg ","/.SertexOS/programs/"..pList[y-2])
