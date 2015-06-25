32af4a8993bdb21e705eaff8e34dd874a4bdad2c
Ale2610 <ale.proto00@gmail.com>
2015-06-25 19:13:50 +0200

    Update programs.lua

--- src/apps/programs/programs.lua
+++ src/apps/programs/programs.lua
@@ -21,15 +21,12 @@ local function draw()
 	local w,h = term.getSize()
 	graphics.line(1,1,w,1, colors.red)
 	term.setTextColor(colors.white)
-	--[[
-	Unused
 	term.setCursorPos(1,1)
 	term.setBackgroundColor(colors.lime)
 	write("+")
 	term.setCursorPos(2,1)
 	term.setBackgroundColor(colors.orange)
 	write("-")
-	]]--
 	term.setCursorPos(w,1)
 	term.setBackgroundColor(colors.red)
 	write("X")
@@ -41,6 +38,7 @@ end
 draw()
 
 while true do
+	local w, h = term.getSize()
 	draw()
 	local pList = fs.list("/.SertexOS/programs")
 	for i = 1, #pList do
@@ -48,6 +46,16 @@ while true do
 	end
 	local e, par1, x, y = os.pullEvent()
 	if e == "mouse_click" or e == "monitor_touch" then
-		shell.run("fg ","/.SertexOS/programs/"..pList[y-2])
+		if y == 1 then
+			if x == w then
+				return
+			elseif x == 1 then
+				
+			elseif x == 2 then
+				
+			end
+		else
+			shell.run("fg ","/.SertexOS/programs/"..pList[y-2])
+		end
 	end
 end
