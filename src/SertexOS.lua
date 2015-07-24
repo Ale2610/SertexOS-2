57efb42b36806ec20340ca9296cda0eaaf8bbc6f
Ale2610 <ale.proto00@gmail.com>
2015-07-24 20:58:20 +0200

    Update SertexOS.lua

--- src/SertexOS.lua
+++ src/SertexOS.lua
@@ -484,7 +484,7 @@ local function desktop()
 		while true do
 			sleep(0)
 			redraw()
-			local sTime = os.startTimer(0.1)
+			local sTime = os.startTimer(0)
 			local ev = {os.pullEventRaw()}
 			if ev[1] == "mouse_click" then
 				local mx = ev[3]
